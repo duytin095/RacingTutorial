2bbd96a953b337bdba931b69cae77064be9566c7
duytin095 <duytin095@gmail.com>
2022-11-20 18:52:46 +0700

    update UI

---
+++ Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    [SerializeField]
+    private float rotateSpeed = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed); 
+    }
+}
