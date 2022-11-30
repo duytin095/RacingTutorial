8f64fa90bff6e585271a7b00c1d56b0c2170673f
duytin095 <duytin095@gmail.com>
2022-11-30 22:22:30 +0700

    material issue fixed

--- Assets/Scripts/CustomCar.cs
+++ Assets/Scripts/CustomCar.cs
@@ -1,14 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CustomCar : MonoBehaviour
 {
     [SerializeField]
     Material carMaterial;
+    [SerializeField]
+    Material defaultMaterial;
+
     private void Start()
     {
         carMaterial = GameManager.Instance.storedColor;
-        transform.GetComponent<MeshRenderer>().material = carMaterial;
+
+        //Get the last time SAVE COLOR and apply this for the car on the scene
+        ChangeMaterial(carMaterial);
+
+        // if can't get the SAVE COLOR
+        if(carMaterial == null) // material dont display right when push git??? or turn off the lap??? dont know why 
+        {
+            ChangeMaterial(defaultMaterial);
+        }
+
+    }
+    private void ChangeMaterial(Material material)
+    {
+        transform.GetComponent<MeshRenderer>().material = material;
     }
 }
